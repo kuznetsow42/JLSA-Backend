9ab46cee08b4d9d37c5442677cc2b173e115434e
kuznetsow42 <kuznetsow42@gmail.com>
2025-01-22 20:56:50 +0500

    fix: remove leak in card serializer

--- jlsa/cards/serializers.py
+++ jlsa/cards/serializers.py
@@ -2,15 +2,16 @@ from rest_framework import serializers
 from .models import Card, Tag
 
 
-class CardSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Card
-        exclude = ["user"]
-        depth = 2
-
-
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
         exclude = ["user"]
+
+
+class CardSerializer(serializers.ModelSerializer):
+    tags = TagSerializer(many=True)
+    class Meta:
+        model = Card
+        exclude = ["user"]
+        depth = 2
 
